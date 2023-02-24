eec1c66fe4c14986c49444a9e9902b7fcd51d333
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2023-02-24 16:48:37 +0700

    Django key debug

--- supaword/secure_env.py
+++ supaword/secure_env.py
@@ -31,30 +31,40 @@ def from_credentials_file():
 
 
 CREDENTIALS = from_credentials_file()
+
+# General
+DB_CONNECTION_STRING = CREDENTIALS.get('CONNECTION_STRING', '') or environment_value('CONNECTION_STRING')
+
+# Supabase
 API_KEY = CREDENTIALS.get('SUPABASE_KEY', '') or environment_value('SUPABASE_KEY')
 DB_URL = CREDENTIALS.get('SUPABASE_URL', '') or environment_value('SUPABASE_URL')
 
+# Postgres
 POSTGRES_PASSWORD = CREDENTIALS.get('POSTGRES_PASSWORD', '') or environment_value('POSTGRES_PASSWORD')
 POSTGRES_ADDRESS = CREDENTIALS.get('POSTGRES_ADDRESS', '') or environment_value('POSTGRES_ADDRESS')
 POSTGRES_USER = CREDENTIALS.get('POSTGRES_USER', '') or environment_value('POSTGRES_USER')
 POSTGRES_DB = CREDENTIALS.get('POSTGRES_DB', '') or environment_value('POSTGRES_DB')
 
-DB_CONNECTION_STRING = CREDENTIALS.get('CONNECTION_STRING', '') or environment_value('CONNECTION_STRING')
-
+# Snaplet
 SNAPLET_SOURCE_DATABASE_URL = CREDENTIALS.get('SNAPLET_SOURCE_DATABASE_URL', '') or environment_value(
     'SNAPLET_SOURCE_DATABASE_URL')
 SNAPLET_DATABASE_URL = CREDENTIALS.get('SNAPLET_DATABASE_URL', '') or environment_value(
     'SNAPLET_DATABASE_URL')
-DJANGO_KEY = CREDENTIALS.get('DJANGO_KEY', '') or environment_value('DJANGO_KEY')
-SERVER_DEBUG = CREDENTIALS.get('DEBUG', '0') or environment_value('DEBUG')
 
+# Django
+DJANGO_KEY = CREDENTIALS.get('DJANGO_KEY', '') or environment_value('DJANGO_KEY')
+
+# Debug
+SERVER_DEBUG = CREDENTIALS.get('DEBUG', '0') or environment_value('DEBUG')
 assert SERVER_DEBUG in (0, 1, "0", "1"), f"DEBUG must be 1 or 0, instead of '{SERVER_DEBUG}'"
 SERVER_DEBUG = bool(int(SERVER_DEBUG))
 
-assert len(DJANGO_KEY) > 0, "Django key is empty"
-assert len(POSTGRES_PASSWORD) > 0, "Database password is empty"
+assert len(DB_CONNECTION_STRING) > 0, "Connection string is empty"
 assert len(POSTGRES_USER) > 0, "Database username is empty"
+assert len(POSTGRES_DB) > 0, "Database name is empty"
+assert len(POSTGRES_PASSWORD) > 0, "Database password is empty"
 assert len(POSTGRES_ADDRESS) > 0, "Postgres address is empty"
+assert len(DJANGO_KEY) > 0, "Django key is empty"
 
 assert DB_CONNECTION_STRING.startswith(
     "postgresql://"
