60cf703d6f465f391f1a3c899913bfe4d7d61c68
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2023-07-22 23:24:05 +0700

    Debug alive flag 2

--- core/views.py
+++ core/views.py
@@ -177,9 +177,9 @@ class PeopleExtendedAPIView(SupawordAPIView):
             )
 
         # Apply the 'alive' filter condition if provided
-        if alive_filter in ['true', 'false']:
+        if alive_filter is not None and alive_filter.tolower() in ['true', 'false']:
             print(f'Case 1 alive_filter = {alive_filter}')
-            people = people.filter(dod__isnull=ast.literal_eval(alive_filter))
+            people = people.filter(dod__isnull=(alive_filter.tolower() == 'true'))
         elif alive_filter in [True, False]:
             print(f'Case 2 alive_filter = {alive_filter}')
             people = people.filter(dod__isnull=alive_filter)
