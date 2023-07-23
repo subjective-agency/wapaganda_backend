984d9e9a8f677b23d82c4d2856c43b00ee13731c
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2023-07-23 13:04:48 +0700

    Implement REST request serializer

--- core/pagination.py
+++ core/pagination.py
@@ -14,17 +14,21 @@ class CustomPostPagination(PageNumberPagination):
         "page_size": 20
     }
     """
+    min_page_size = 10
+    max_page_size = 100
     page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
 
     def get_page_number(self, request, paginator, view=None):
         """
-        Get the page number from the request
+        Get the validated page number from the request
         :param request: Object of type rest_framework.request.Request
         :param paginator: Object of type rest_framework.pagination.PageNumberPagination
         :param view: Object of type rest_framework.viewsets.ViewSet
         :return: Page number
         """
         page_number = request.data.get('page', 0)
+        if not isinstance(page_number, int):
+            raise ValueError(f"Invalid page_number {page_number} type; must be an integer")
         if page_number == 0:
             page_number = request.query_params.get('page', 0)
         if page_number == 0:
@@ -33,9 +37,18 @@ class CustomPostPagination(PageNumberPagination):
 
     def get_page_size(self, request):
         """
-        Get the page size from the request
+        Get the validated page size from the request
         """
-        return request.data.get('page_size', request.query_params.get('page_size', self.page_size))
+        page_size = request.data.get('page_size', request.query_params.get('page_size', self.page_size))
+        if not isinstance(page_size, int):
+            raise ValueError(f"Invalid page_size {page_size} type; must be an integer")
+        if page_size < 1:
+            raise ValueError(f"Invalid page_size {page_size} size; must be a positive integer")
+        elif page_size < self.min_page_size:
+            page_size = self.min_page_size
+        elif page_size > self.max_page_size:
+            page_size = self.max_page_size
+        return page_size
 
     def get_paginated_response(self, data):
         """
@@ -47,4 +60,4 @@ class CustomPostPagination(PageNumberPagination):
             'total': self.page.paginator.count,
             'page_size': CustomPostPagination.page_size,
             'page': data
-        }, headers={'Server-Version': settings.VERSION})
+        })
--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,12 @@
+import re
+from abc import ABC
+
 from . import models
 from . import fields
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 
 
 class PeopleExtendedBriefSerializer(serializers.Serializer):
@@ -277,3 +283,73 @@ class PeopleInOrgsSerializer(serializers.Serializer):
             'year_started',
             'year_ended'
         )
+
+
+class CommonRequestSerializer(serializers.Serializer, ABC):
+
+    @staticmethod
+    def tristate_param(param):
+        """
+        Convert a parameter to tristate value (True, False, None)
+        """
+        if param is None:
+            return None
+        if isinstance(param, bool):
+            return param
+        if isinstance(param, str) and param.lower() in ['true', 'false']:
+            return param.lower() == 'true'
+        raise ValueError(f'Invalid tristate value [{param}]')
+
+
+# noinspection PyMethodMayBeStatic
+class PagingRequestSerializer(CommonRequestSerializer):
+
+    def update(self, instance, validated_data):
+        """
+        We do not manage the update of the data
+        """
+        pass
+
+    def create(self, validated_data):
+        """
+        We do not manage the creation of the data
+        """
+        pass
+
+    type = serializers.CharField(default='page', read_only=True)
+    page = serializers.IntegerField(required=True, read_only=True, min_value=1)
+    page_size = serializers.IntegerField(required=True, read_only=True, min_value=10, max_value=100)
+    sort_by = serializers.ChoiceField(choices=['id', 'fullname_en', 'fullname_ru', 'fullname_uk', 'dob', 'dod', 'sex'],
+                                      required=False,
+                                      default='id')
+    sort_direction = serializers.ChoiceField(choices=['asc', 'desc'], required=False, default='asc')
+    filter = serializers.CharField(required=False, allow_blank=True, allow_null=True, max_length=100)
+    alive = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+
+    def validate_type(self, value):
+        if value.lower() != 'page':
+            raise ValidationError('Invalid request type, "page" expected')
+        return value
+
+    def validate_filter(self, value):
+        """
+        Validate the filter field
+        """
+        # Allow empty or null value
+        if value is None or value.strip() == '':
+            return value
+
+        # Use a regular expression to check if the filter is a valid wildcard mask
+        # (e.g., "john*", "*smith", "j?hn", etc.)
+        valid_wildcard_mask = re.compile(r'^[\w\s*?]+$')
+
+        if not valid_wildcard_mask.match(value):
+            raise ValidationError('Invalid filter format: it must be a valid wildcard mask or empty')
+
+        return value
+
+    def validate_alive(self, value):
+        """
+        Validate the alive field
+        """
+        return self.tristate_param(value)
