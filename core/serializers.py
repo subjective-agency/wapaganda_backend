cd7d9302d30690f75616bd88bf67e00e9d9c5b1f
Yurii Cherkasov <strategarius@protonmail.com>
2023-08-06 14:40:03 +0700

    Merge models

--- core/serializers.py
+++ core/serializers.py
@@ -7,6 +7,39 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 
+class TheorySerializer(serializers.Serializer):
+    """
+    Serializer for Theory table, containing relatively large articles
+    """
+    id = serializers.IntegerField()
+    title = serializers.JSONField(allow_null=True)
+    type = serializers.CharField(allow_blank=True, allow_null=True)
+    excerpt = serializers.JSONField(allow_null=True)
+    content = serializers.JSONField(allow_null=True)
+
+    def create(self, validated_data):
+        """
+        We do not manage the creation of the data
+        """
+        pass
+
+    def update(self, instance, validated_data):
+        """
+        We do not manage the update of the data
+        """
+        pass
+
+    class Meta:
+        model = models.Theory
+        fields = (
+            'id',
+            'title',
+            'type',
+            'excerpt',
+            'content'
+        )
+
+
 class PeopleExtendedBriefSerializer(serializers.Serializer):
     """
     Serializer to send a response back to user.
@@ -94,18 +127,14 @@ class PeopleExtendedSerializer(serializers.Serializer):
     lastname_en = serializers.CharField(allow_blank=True, allow_null=True)
     lastname_ru = serializers.CharField(allow_blank=True, allow_null=True)
     social = serializers.CharField(allow_blank=True, allow_null=True)
-    # social = serializers.ListField(child=serializers.CharField(allow_blank=True, allow_null=True))
     dob = serializers.DateField(allow_null=True)
     is_ttu = serializers.BooleanField(allow_null=True)
     is_ff = serializers.BooleanField(allow_null=True)
     contact = serializers.JSONField(allow_null=True)
     address = serializers.CharField(allow_blank=True, allow_null=True)
-    # address = serializers.ListField(child=serializers.CharField(allow_blank=True, allow_null=True))
     associates = serializers.JSONField(allow_null=True)
-    # associates = serializers.ListField(child=serializers.JSONField(allow_blank=True, allow_null=True))
     additional = serializers.JSONField(allow_null=True)
     aliases = serializers.JSONField(allow_null=True)
-    # aliases = serializers.ListField(child=serializers.JSONField(allow_blank=True, allow_null=True))
     info = serializers.JSONField(allow_null=True)
     dod = serializers.DateField(allow_null=True)
     cod = serializers.CharField(allow_blank=True, allow_null=True)
@@ -114,16 +143,12 @@ class PeopleExtendedSerializer(serializers.Serializer):
     photo = serializers.CharField(allow_blank=True, allow_null=True)
     external_links = serializers.CharField(allow_blank=True, allow_null=True)
     bundles = serializers.JSONField(allow_null=True)
-    # bundles = serializers.ListField(child=serializers.JSONField(allow_blank=True, allow_null=True))
     thumb = serializers.CharField(allow_blank=True, allow_null=True)
     added_on = serializers.DateTimeField(allow_null=False)
     sex = serializers.CharField(allow_null=True)
     orgs = serializers.JSONField(allow_null=True)
-    # orgs = serializers.ListField(child=serializers.JSONField(allow_blank=True, allow_null=True))
     telegram_channels = serializers.JSONField(allow_null=True)
-    # telegram_channels = serializers.ListField(child=serializers.JSONField(allow_blank=True, allow_null=True))
     youtube_channels = serializers.JSONField(allow_null=True)
-    # youtube_channels = serializers.ListField(child=serializers.JSONField(allow_blank=True, allow_null=True))
 
     def create(self, validated_data):
         """
@@ -281,93 +306,3 @@ class PeopleInOrgsSerializer(serializers.Serializer):
             'year_started',
             'year_ended'
         )
-
-
-class CommonRequestSerializer(serializers.Serializer):
-
-    @abstractmethod
-    def validate_type(self, value):
-        """
-        Abstract method for validating the "type" field
-        Each concrete subclass must implement this method
-        """
-        raise NotImplementedError('You must implement `validate_type()` in a subclass')
-
-    def update(self, instance, validated_data):
-        """
-        We do not manage the update of the data
-        """
-        pass
-
-    def create(self, validated_data):
-        """
-        We do not manage the creation of the data
-        """
-        pass
-
-    @staticmethod
-    def tristate_param(param):
-        """
-        Convert a parameter to tristate value (True, False, None)
-        """
-        if param is None:
-            return None
-        if isinstance(param, bool):
-            return param
-        if isinstance(param, str) and param.lower() in ['true', 'false']:
-            return param.lower() == 'true'
-        raise ValueError(f'Invalid tristate value [{param}]')
-
-
-# noinspection PyMethodMayBeStatic
-class PagingRequestSerializer(CommonRequestSerializer):
-
-    def update(self, instance, validated_data):
-        """
-        We do not manage the update of the data
-        """
-        pass
-
-    def create(self, validated_data):
-        """
-        We do not manage the creation of the data
-        """
-        pass
-
-    type = serializers.CharField(required=True)
-    page = serializers.IntegerField(required=True, min_value=0)
-    page_size = serializers.IntegerField(required=True, min_value=9, max_value=100)
-    sort_by = serializers.ChoiceField(choices=['id', 'fullname_en', 'fullname_ru', 'fullname_uk', 'dob', 'dod', 'sex'],
-                                      required=False,
-                                      default='id')
-    sort_direction = serializers.ChoiceField(choices=['asc', 'desc'], required=False, default='asc')
-    filter = serializers.CharField(required=False, allow_blank=True, allow_null=True, max_length=100)
-    alive = serializers.BooleanField(required=False, allow_null=True)
-
-    def validate_type(self, value):
-        if value.lower() != 'page':
-            raise ValidationError('Invalid request type, "page" expected')
-        return value
-
-    def validate_filter(self, value):
-        """
-        Validate the filter field
-        """
-        # Allow empty or null value
-        if value is None or value.strip() == '':
-            return value
-
-        # Use a regular expression to check if the filter is a valid wildcard mask
-        # (e.g., "john*", "*smith", "j?hn", etc.)
-        valid_wildcard_mask = re.compile(r'^[\w\s*?]+$')
-
-        if not valid_wildcard_mask.match(value):
-            raise ValidationError('Invalid filter format: it must be a valid wildcard mask or empty')
-
-        return value
-
-    def validate_alive(self, value):
-        """
-        Validate the alive field
-        """
-        return self.tristate_param(value)
