f29e1986214eca03f18c7536ccccd2869eb49633
Yurii Cherkasov <strategarius@protonmail.com>
2023-08-21 15:10:39 +0700

    Return cache exclude 7

--- core/views.py
+++ core/views.py
@@ -141,8 +141,8 @@ class PeopleExtendedAPIView(SupawordAPIView):
         if request.data.get('type', '') != 'cache':
             return Response({'error': 'Invalid request type, "cache" expected'}, status=status.HTTP_400_BAD_REQUEST)
         request_data = request.data
-        created_after = request_data.get('timestamp', 0)
-        created_after_datetime = datetime.fromtimestamp(created_after, tz=timezone.utc) + 1
+        created_after = request_data.get('timestamp', 0) + 1
+        created_after_datetime = datetime.fromtimestamp(created_after, tz=timezone.utc)
 
         # All records created after the specified timestamp
         people = PeopleExtended.objects.filter(added_on__gt=created_after_datetime).order_by('id')
