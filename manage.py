6b26a455efe8f594cb2a50bc26e8041b83ded921
Yurii Cherkasov <strategarius@protonmail.com>
2023-08-25 15:16:46 +0700

    manage.py exportdata

--- manage.py
+++ manage.py
@@ -4,49 +4,76 @@ import os
 import sys
 import urllib.request
 from urllib.error import HTTPError
+from supaword.secure_env import POSTGRES_PASSWORD, POSTGRES_ADDRESS, POSTGRES_PORT, POSTGRES_USER, POSTGRES_DB
+from tools.import_db import PostgresDbExport, TABLE_NAMES
+from supaword.log_helper import logger
 import zipfile
 import io
 
-FRONTEND_VERSION = '0.9.3'
 URLS = [
     f'https://svfizyfozagyqkkjzqdc.supabase.co/storage/v1/object/public/packages/frontend/',
     f'https://wapaganda-frontend.s3.amazonaws.com/builds/'
 ]
 
 
-def fetch_static(urls, version):
+def fetch_static(version):
     """
     Fetch static files from the given urls, unpack to static if successful
     """
-    for url in urls:
+    for url in URLS:
         archive_name = f'wapaganda-frontend-{version}.zip'
         archive_url = f'{url}{archive_name}'
 
         try:
-            print(f"Try fetching {archive_url}")
+            logger.info(f"Try fetching {archive_url}")
             response = urllib.request.urlopen(archive_url)
 
             if response.status == 200:
-                print(f"Extracting zip from {archive_url}")
+                logger.info(f"Extracting zip from {archive_url}")
                 with zipfile.ZipFile(io.BytesIO(response.read())) as archive:
                     archive.extractall("static")
                 extracted_files = os.listdir("static/build")
                 extracted_to = os.path.abspath("static/build")
-                print(f"Successfully extracted to {extracted_to} following static files: {extracted_files}")
+                logger.info(f"Successfully extracted to {extracted_to} following static files: {extracted_files}")
                 return
             else:
-                print(f"Failed to fetch static files, trying the next. Status code: {response.status}")
+                logger.info(f"Failed to fetch static files, trying the next. Status code: {response.status}")
                 continue
 
         except HTTPError as e:
-            print(f"Failed to fetch static files, trying the next. Return code {e.code}")
+            logger.info(f"Failed to fetch static files, trying the next. Return code {e.code}")
             continue
+
+
+def export_data():
+    """
+    Export data from Postgres database to JSON files
+    """
+    db_export = PostgresDbExport(
+        dbname=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=POSTGRES_ADDRESS,
+        port=POSTGRES_PORT
+    )
+    db_export.export_to_json(TABLE_NAMES)
 
 
 def main():
     """
     Run administrative tasks
     """
+    command_handlers = {
+        "fetchstatic": {
+            "handle": fetch_static,
+            "params": 2
+        },
+        "exportdata": {
+            "handle": export_data,
+            "params": 1
+        }
+    }
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supaword.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -57,10 +84,12 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
 
-    if len(sys.argv) > 1 and sys.argv[1] == 'fetchstatic':
-        version = sys.argv[2] if len(sys.argv) > 2 else FRONTEND_VERSION
-        fetch_static(URLS, version)
-        sys.exit(0)
+    command = sys.argv[1] if len(sys.argv) > 1 else None
+    if command in command_handlers:
+        if len(sys.argv) - 2 == command_handlers[command]["params"]:
+            command_handlers[command]["handle"](*sys.argv[2:])
+        else:
+            logger.error(f"Invalid number of parameters for command '{command}'")
 
     execute_from_command_line(sys.argv)
 
---
+++ tools/import_db.py
@@ -0,0 +1,105 @@
+import psycopg2
+import json
+from supaword.log_helper import logger
+
+
+TABLE_NAMES = [
+    "days_of_war",
+    "komso_episodes",
+    "media_coverage_type",
+    "media_roles",
+    "media_segments",
+    "msegments_to_rchannels_mapping",
+    "msegments_to_ychannels_mapping",
+    "ntv_episodes",
+    "organization_type",
+    "organizations",
+    "people",
+    "people_3rdprt_details_raw",
+    "people_bundles",
+    "people_extended",
+    "people_in_bundles",
+    "people_in_orgs",
+    "people_in_ur",
+    "people_on_photos",
+    "people_on_smotrim",
+    "people_on_youtube",
+    "people_to_msegments_mapping",
+    "photos",
+    "printed",
+    "printed_to_people_mapping",
+    "quotes",
+    "rutube_channels",
+    "rutube_vids",
+    "smotrim_episodes",
+    "telegram_authors",
+    "telegram_channels",
+    "theory",
+    "websites",
+    "youtube_authors",
+    "youtube_channels",
+    "youtube_vids"
+]
+
+
+class PostgresDbExport:
+    def __init__(self, dbname: str, user: str, password: str, host: str, port: int):
+        """
+        :param dbname:
+        :param user:
+        :param password:
+        :param host:
+        :param port:
+        """
+        assert len(dbname) > 0, "Database name is empty"
+        assert len(user) > 0, "Database username is empty"
+        assert len(password) > 0, "Database password is empty"
+        assert len(host) > 0, "Database host is empty"
+        assert port > 0, "Database port is empty"
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+
+    @staticmethod
+    def _export_table(connection, table_name):
+        cursor = connection.cursor()
+        query = f"SELECT * FROM {table_name}"
+
+        try:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+
+            json_filename = f"{table_name}.json"
+            with open(json_filename, "w") as json_file:
+                json.dump(rows, json_file)
+
+            logger.info(f"Table {table_name} exported to {json_filename}")
+        except Exception as e:
+            logger.error(f"Error exporting table {table_name}: {e}")
+        finally:
+            cursor.close()
+
+    def export_to_json(self, table_names: list):
+        """
+        Export data from Postgres database to JSON files
+        :param table_names:
+        """
+        connection = None
+        try:
+            connection = psycopg2.connect(
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port
+            )
+
+            for table_name in table_names:
+                self._export_table(connection, table_name)
+        except Exception as e:
+            logger.error(f"Error: {e}")
+        finally:
+            if connection:
+                connection.close()
