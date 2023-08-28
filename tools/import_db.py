a92c3859fdf74029ea683e8aa1bd7e64d8020071
Yurii Cherkasov <strategarius@protonmail.com>
2023-08-28 16:08:20 +0700

    manage.py import_data: sorting

--- tools/import_db.py
+++ tools/import_db.py
@@ -58,10 +58,14 @@ class PostgresDbImport:
             logger.error(f"Error loading data from {json_filename}: {e}")
             return
 
-    def import_tables(self, json_files):
+    def import_tables(self, table_names):
         """
         Import data from JSON files to Postgres database
         """
+        json_files = [
+            os.path.join(os.path.abspath("tools/import"), f"{table_name}.json") for table_name in table_names
+        ]
+        logger.info(f"Importing data from JSON files: {json_files}")
         try:
             self.connection = psycopg2.connect(
                 dbname=self.dbname,
@@ -78,56 +82,3 @@ class PostgresDbImport:
         finally:
             if self.connection:
                 self.connection.close()
-
-    def drop_tables(self, table_names):
-        try:
-            self.connection = psycopg2.connect(
-                dbname=self.dbname,
-                user=self.user,
-                password=self.password,
-                host=self.host,
-                port=self.port
-            )
-
-            cursor = self.connection.cursor()
-            drop_command = f"DROP TABLE IF EXISTS {', '.join(table_names)} CASCADE"
-            cursor.execute(drop_command)
-            self.connection.commit()
-
-            for table_name in table_names:
-                logger.info(f"Table {table_name} dropped")
-        except Exception as e:
-            logger.error(f"Error: {e}")
-        finally:
-            if self.connection:
-                self.connection.close()
-
-    def _truncate_table(self, table_name):
-        try:
-            cursor = self.connection.cursor()
-            truncate_command = f"TRUNCATE TABLE {table_name} CASCADE"
-            cursor.execute(truncate_command)
-            self.connection.commit()
-            logger.info(f"Table {table_name} truncated")
-        except Exception as e:
-            logger.error(f"Error truncating table {table_name}: {e}")
-            self.connection.rollback()
-
-    def truncate_tables(self, table_names):
-        try:
-            self.connection = psycopg2.connect(
-                dbname=self.dbname,
-                user=self.user,
-                password=self.password,
-                host=self.host,
-                port=self.port
-            )
-
-            for table_name in table_names:
-                self._truncate_table(table_name)
-        except Exception as e:
-            logger.error(f"Error: {e}")
-        finally:
-            if self.connection:
-                self.connection.close()
-
