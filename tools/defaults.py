42296ad2fd9a4d2446edc9556ab2b5ea5b85ff8f
Yurii Cherkasov <strategarius@protonmail.com>
2023-09-14 19:13:21 +0700

    Update table list 4

--- tools/defaults.py
+++ tools/defaults.py
@@ -1,42 +1,3 @@
-TABLE_NAMES = [
-    "days_of_war",
-    "komso_episodes",
-    "media_coverage_type",
-    "media_roles",
-    "media_segments",
-    "msegments_to_rchannels_mapping",
-    "msegments_to_ychannels_mapping",
-    "ntv_episodes",
-    "organization_type",
-    "organizations",
-    "people",
-    "people_3rdprt_details_raw",
-    "people_bundles",
-    "people_extended",
-    "people_in_bundles",
-    "people_in_orgs",
-    "people_in_ur",
-    "people_on_photos",
-    "people_on_smotrim",
-    "people_on_youtube",
-    "people_to_msegments_mapping",
-    "photos",
-    "printed",
-    "printed_to_people_mapping",
-    "quotes",
-    "rutube_channels",
-    "rutube_vids",
-    "smotrim_episodes",
-    "telegram_authors",
-    "telegram_channels",
-    "theory",
-    "websites",
-    "youtube_authors",
-    "youtube_channels",
-    "youtube_vids"
-]
-
-
 def read_table_names(table_names_file):
     """
     Read table names from file
@@ -44,5 +5,5 @@ def read_table_names(table_names_file):
     table_names = []
     if table_names_file is not None:
         with open(table_names_file, "r") as file:
-            [table_names.append(line.strip()) for line in file.readlines() if line.strip() in TABLE_NAMES]
+            [table_names.append(line.strip()) for line in file.readlines()]
     return table_names
