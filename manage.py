f03f4859078ac4bae14c82b539f43fc156d280c9
Yurii Cherkasov <strategarius@protonmail.com>
2023-09-15 16:11:57 +0700

    Remove fetch static option

--- manage.py
+++ manage.py
@@ -2,50 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import urllib.request
-from urllib.error import HTTPError
 from supaword.secure_env import POSTGRES_PASSWORD, POSTGRES_ADDRESS, POSTGRES_PORT, POSTGRES_USER, POSTGRES_DB
 from tools.defaults import read_table_names
 from tools.export_db import PostgresDbExport
 from tools.import_db import PostgresDbImport
 from tools.cleanup_db import PostgresDbCleanup
 from supaword.log_helper import logger
-import zipfile
-import io
-
-URLS = [
-    f'https://svfizyfozagyqkkjzqdc.supabase.co/storage/v1/object/public/packages/frontend/',
-    f'https://wapaganda-frontend.s3.amazonaws.com/builds/'
-]
-
-
-def fetch_static(version):
-    """
-    Fetch static files from the given urls, unpack to static if successful
-    """
-    for url in URLS:
-        archive_name = f'wapaganda-frontend-{version}.zip'
-        archive_url = f'{url}{archive_name}'
-
-        try:
-            logger.info(f"Try fetching {archive_url}")
-            response = urllib.request.urlopen(archive_url)
-
-            if response.status == 200:
-                logger.info(f"Extracting zip from {archive_url}")
-                with zipfile.ZipFile(io.BytesIO(response.read())) as archive:
-                    archive.extractall("static")
-                extracted_files = os.listdir("static/build")
-                extracted_to = os.path.abspath("static/build")
-                logger.info(f"Successfully extracted to {extracted_to} following static files: {extracted_files}")
-                return
-            else:
-                logger.info(f"Failed to fetch static files, trying the next. Status code: {response.status}")
-                continue
-
-        except HTTPError as e:
-            logger.info(f"Failed to fetch static files, trying the next. Return code {e.code}")
-            continue
 
 
 def export_data(table_names_file=None, rewrite=True):
@@ -127,10 +89,6 @@ def main():
     Run administrative tasks
     """
     command_handlers = {
-        "fetch_static": {
-            "handle": fetch_static,
-            "params": ["version"]
-        },
         "export_data": {
             "handle": export_data,
             "params": ["table_names_file", "rewrite"]
