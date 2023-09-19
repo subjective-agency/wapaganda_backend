11e4a1b5c1fa69c01038f16ea931a25c347a1cac
Yurii Cherkasov <strategarius@protonmail.com>
2023-09-19 18:25:12 +0700

    Debug

--- tools/attempt_select.py
+++ tools/attempt_select.py
@@ -8,16 +8,17 @@ def connect_to_database(credentials_file):
         with open(credentials_file, 'r') as file:
             credentials = json.load(file)
             conn = psycopg2.connect(
-                dbname=credentials['dbname'],
-                user=credentials['user'],
-                password=credentials['password'],
-                host=credentials['host'],
-                port=credentials['port']
+                dbname=credentials['POSTGRES_DB'],
+                user=credentials['POSTGRES_USER'],
+                password=credentials['POSTGRES_PASSWORD'],
+                host=credentials['POSTGRES_ADDRESS'],
+                port=credentials['POSTGRES_PORT']
             )
             return conn
     except Exception as e:
         print(f"Error connecting to the database: {e}")
         return None
+
 
 def execute_query(connection, query):
     try:
@@ -32,20 +33,21 @@ def execute_query(connection, query):
 
 def main():
     parser = argparse.ArgumentParser(description="Execute a SELECT query on a PostgreSQL table.")
-    parser.add_argument("--credentials", required=True, help="Path to the JSON credentials file.")
-    parser.add_argument("--table", required=True, help="Name of the table to query.")
+    parser.add_argument("--credentials",
+                        required=False,
+                        help="Path to the JSON credentials file",
+                        default="credentials.json")
+    parser.add_argument("--table",
+                        required=True,
+                        help="Name of the table to query")
     args = parser.parse_args()
 
     connection = connect_to_database(args.credentials)
     if connection:
         query = f"SELECT * FROM {args.table};"
         rows = execute_query(connection, query)
-
         if rows:
-            print("Query results:")
-            for row in rows:
-                print(row)
-        
+            print(f"Received {len(rows)} records")
         connection.close()
 
 
