b7179f98f750094d134ac7f2d08b6ecf66010c79
Yurii Cherkasov <strategarius@protonmail.com>
2023-09-26 15:38:33 +0700

    Remove org endpoint

--- supaword/urls.py
+++ supaword/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('', core_views.bad_request),
     path('admin/', admin.site.urls),
     path('people/', core_views.PeopleExtendedAPIView.as_view()),
-    path('org/', core_views.OrganizationsAPIView.as_view()),
     path('theory/', core_views.TheoryAPIView.as_view())
 ]
