ca5f4f5b1360c17c15a577d8d9037e115f15af5f
Yurii Cherkasov <strategarius@protonmail.com>
2023-09-27 12:21:12 +0700

    Theory validation 2

--- core/management/commands/validate_theory.py
+++ core/management/commands/validate_theory.py
@@ -3,8 +3,7 @@
 import os
 from django.core.management.base import BaseCommand, CommandError
 from supaword.secure_env import POSTGRES_PASSWORD, POSTGRES_ADDRESS, POSTGRES_PORT, POSTGRES_USER, POSTGRES_DB
-from tools.utils import read_table_names
-from tools.export_db import PostgresDbExport
+from tools.validate_table import ValidateTable
 from supaword.log_helper import logger
 
 
@@ -13,26 +12,25 @@ class Command(BaseCommand):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.table_names = ""
-        self.bool_rewrite_tables = False
-        self.bool_continue_export = False
+        self.table_name = ""
 
     def add_arguments(self, parser):
-        parser.add_argument('table_name', type=str, help='File containing table names')
+        parser.add_argument('table_name', type=str, help='table name to validate')
 
     def validate_theory(self):
         """
         Export data from Postgres database to JSON files
         """
-        theory_validator = ValidateTheoryTable(
+        validator = ValidateTable(
             dbname=POSTGRES_DB,
             user=POSTGRES_USER,
             password=POSTGRES_PASSWORD,
             host=POSTGRES_ADDRESS,
             port=POSTGRES_PORT
         )
-        theory_validator.connect_to_database()
-        theory_validator.validate_metadata_dates()
+        validator.connect_to_database()
+        validator.validate_table(self.table_name)
+        validator.close_connection()
 
     def handle(self, *args, **options):
         self.table_name = options['table_names_file']
@@ -42,5 +40,4 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS(
             f'Successfully validated table: {self.table_name}'
         ))
-
     
--- tools/validate_table.py
+++ tools/validate_table.py
@@ -1,7 +1,6 @@
 import psycopg2
-import os
 
-class ValidateTheoryTable:
+class ValidateTable:
     def __init__(self, dbname: str, user: str, password: str, host: str, port: int):
         self.db_params = {
             "dbname": dbname,
@@ -10,8 +9,14 @@ class ValidateTheoryTable:
             "host": host,
             "port": port
         }
-        self.script_dir = os.path.dirname(os.path.abspath(__file__))
-        self.log_file_path = os.path.join(self.script_dir, "error_log.txt")
+        self.query_dict = {
+            "theory": f"""
+                UPDATE theory
+                SET original_content_metadata = original_content_metadata
+                WHERE publish_date IS NOT NULL;
+            """
+            # Add more tables and their corresponding queries here
+        }
 
     def connect_to_database(self):
         try:
@@ -21,27 +26,24 @@ class ValidateTheoryTable:
             print("Error connecting to the database:", e)
             exit()
 
-    def validate_metadata_dates(self):
+    def validate_table(self, table_name):
         try:
-            self.cursor.execute("""
-                SELECT id, original_content_metadata->>'date'
-                FROM theory
-                WHERE original_content_metadata->>'date' ~ '\d{4}-\d{2}-\d{2}'
-            """)
-            
-            with open(self.log_file_path, "a") as log_file:
-                for row in self.cursor.fetchall():
-                    theory_id, date_str = row
-                    try:
-                        # Attempt to parse the date; if it fails, log the error to the file
-                        error_message = f"Date inconsistency for theory ID {theory_id}: {date_str}\n"
-                        log_file.write(error_message)
-                    except Exception as e:
-                        print(f"Error logging error to file: {e}")
-
+            sql_query = self.query_dict.get(table_name)
+            if sql_query:
+                self.cursor.execute(sql_query)
+                self.connection.commit()
+                print(f"Validation trigger executed for table: {table_name}")
+            else:
+                print(f"No validation query found for table: {table_name}")
         except psycopg2.Error as e:
-            print("Error querying the database:", e)
+            print(f"Error executing validation trigger for table {table_name}:", e)
 
-        finally:
-            self.cursor.close()
-            self.connection.close()
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
+
+# Example usage:
+# validator = ValidateTable("your_db_name", "your_db_user", "your_db_password", "your_db_host", your_db_port)
+# validator.connect_to_database()
+# validator.validate_table("theory")
+# validator.close_connection()
