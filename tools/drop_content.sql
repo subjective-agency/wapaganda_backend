884034282b5d61ae32a0bed1d930f041d8305347
Yurii Cherkasov <strategarius@protonmail.com>
2023-10-12 17:22:28 +0700

    Dependencies tree update wip

--- tools/drop_content.sql
+++ tools/drop_content.sql
@@ -14,26 +14,52 @@ drop table if exists printed_to_people_mapping;
 drop table if exists printed;
 drop table if exists telegram_authors;
 drop table if exists telegram_channels;
-drop table if exists days_of_war;
 drop table if exists people_bundles;
 drop table if exists photos;
 drop table if exists quotes;
 drop table if exists theory;
+drop table if exists media_roles;
+drop table if exists organization_type;
+drop table if exists media_coverage_type;
+drop table if exists text_media;
 drop table if exists websites;
-drop table if exists people;
 drop table if exists rutube_vids;
-drop table if exists youtube_vids;
+drop table if exists rutube_channels;
 drop table if exists smotrim_episodes;
 drop table if exists komso_episodes;
 drop table if exists ntv_episodes;
-drop table if exists media_roles;
+drop table if exists youtube_authors;
+drop table if exists printed_to_people_mapping;
+drop table if exists printed;
+drop table if exists telegram_authors;
+drop table if exists telegram_channels;
+drop table if exists people;
 drop table if exists people_extended;
-drop table if exists rutube_channels;
+
+
+drop table if exists youtube_vids;
 drop table if exists youtube_channels;
 drop table if exists media_segments;
 drop table if exists organizations;
-drop table if exists organization_type;
-drop table if exists media_coverage_type;
+
+drop table if exists future_rodniki;
+
+drop table if exists data_transcribed_content;
+drop table if exists data_transcribed_content_translation_en;
+drop table if exists data_telegram_channels_stats;
+drop table if exists data_telegram_messages;
+drop table if exists data_transcripts;
+drop table if exists data_printed_content;
+
+drop table if exists enums_rucr_taxonomy;
+drop table if exists enums_isco08_index;
+drop table if exists enums_theory_types;
+drop table if exists enums_isco08_taxonomy_closure;
+drop table if exists enums_orgs_taxonomy_closure;
+drop table if exists enums_isco08_taxonomy;
+drop table if exists enums_bundle_types;
+
+drop table if exists enums_orgs_taxonomy;
 
 drop table if exists django_admin_log;
 drop table if exists django_migrations;
