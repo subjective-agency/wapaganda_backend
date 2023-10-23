2697f612eb49e7331954c500cf7fe63bba4b25ba
Yurii Cherkasov <strategarius@protonmail.com>
2023-10-23 17:45:41 +0700

    Restore shell script 01

--- tools/migration/snaplet_restore.sh
+++ tools/migration/snaplet_restore.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Restore wapadb without service tables
+
 snaplet snapshot restore --exclude-tables auth.audit_log_entries auth.identities auth.instances auth.mfa_amr_claims auth.mfa_challenges auth.mfa_factors auth.refresh_tokens auth.saml_providers auth.saml_relay_states auth.schema_migrations auth.sessions auth.sso_domains auth.sso_providers auth.users auth.flow_state supabase_migrations.schema_migrations supabase_function.hooks supabase_function.migrations cron.job cron.job_run_details
