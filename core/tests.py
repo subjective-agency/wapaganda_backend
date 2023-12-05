2a9ae6c449d85b4d652f8928385b8782404110d3
betterthanever2 <68774738+betterthanever2@users.noreply.github.com>
2023-12-05 11:11:40 +0200

    Import specific view and model; endpoint name updated

--- core/tests.py
+++ core/tests.py
@@ -3,8 +3,8 @@ from hypothesis.strategies import integers, dictionaries, text
 from rest_framework.test import APIRequestFactory, force_authenticate
 from rest_framework.request import Request
 from .pagination import CustomPostPagination
-from .views import YourViewSet  # TODO?
-from .models import YourModel  # TODO?
+from .views import PeopleExtendedAPIView
+from .models import PeopleExtended
 from django.test import TestCase
 
 # In this example, we are using Hypothesis to generate random dictionaries with keys of type string and values of type integer. These dictionaries are used as the data for the POST request. The `given` decorator is used to specify the input data for the test.
@@ -13,16 +13,18 @@ from django.test import TestCase
 #
 # Remember to run your tests with a command like `python manage.py test`.
 
+
 class TestCustomPostPagination(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.view = YourViewSet.as_view({'post': 'list'})
+        # self.view = PeopleExtendedAPIView.as_view({'post': 'list'})
+        self.view = PeopleExtendedAPIView.as_view()
         self.pagination = CustomPostPagination()
-        self.user = YourModel.objects.create_user('test', 'test@test.com', 'test')
+        self.user = PeopleExtended.objects.create_user('test', 'test@test.com', 'test')
 
     @given(dictionaries(text(min_size=5, max_size=20), integers(min_value=1, max_value=100)))
     def test_get_page_number(self, data):
-        request = self.factory.post('/api/your_endpoint/', data, format='json')
+        request = self.factory.post('/api/people/', data, format='json')
         force_authenticate(request, user=self.user)
         request = Request(request)
         page_number = self.pagination.get_page_number(request, self.pagination)
@@ -31,7 +33,7 @@ class TestCustomPostPagination(TestCase):
 
     @given(dictionaries(text(min_size=5, max_size=20), integers(min_value=1, max_value=100)))
     def test_get_page_size(self, data):
-        request = self.factory.post('/api/your_endpoint/', data, format='json')
+        request = self.factory.post('/api/people/', data, format='json')
         force_authenticate(request, user=self.user)
         request = Request(request)
         page_size = self.pagination.get_page_size(request)
