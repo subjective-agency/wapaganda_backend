940a9102c6da1a0be2ebfb2924144da1be4a0c2b
Yurii Cherkasov <strategarius@protonmail.com>
2023-12-07 10:18:14 +0700

    Hypothesis

--- core/tests.py
+++ core/tests.py
@@ -1,42 +1,72 @@
-from hypothesis import given
-from hypothesis.strategies import integers, dictionaries, text
-from rest_framework.test import APIRequestFactory, force_authenticate
-from rest_framework.request import Request
-from .pagination import CustomPostPagination
-from .views import PeopleExtendedAPIView
-from .models import PeopleExtended
+from hypothesis import given, strategies as st
+from rest_framework.test import APIClient
+from rest_framework import status
 from django.test import TestCase
 
-# In this example, we are using Hypothesis to generate random dictionaries with keys of type string and values of type integer. These dictionaries are used as the data for the POST request. The `given` decorator is used to specify the input data for the test.
-#
-# Please replace `YourViewSet` and `YourModel` with the actual ViewSet and Model you are using in your project. Also, replace `/api/your_endpoint/` with the actual endpoint you are testing.
-#
-# Remember to run your tests with a command like `python manage.py test`.
+__doc__ = """In this example, we are using Hypothesis to generate random dictionaries 
+with keys of type string and values of type integer. 
+These dictionaries are used as the data for the POST request. 
+The `given` decorator is used to specify the input data for the test.
+Remember to run your tests with a command like `python manage.py test`.
+"""
 
 
-class TestCustomPostPagination(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        # self.view = PeopleExtendedAPIView.as_view({'post': 'list'})
-        self.view = PeopleExtendedAPIView.as_view()
-        self.pagination = CustomPostPagination()
-        self.user = PeopleExtended.objects.create_user('test', 'test@test.com', 'test')
+class PeopleExtendedApiTestCase(TestCase):
 
-    @given(dictionaries(text(min_size=5, max_size=20), integers(min_value=1, max_value=100)))
-    def test_get_page_number(self, data):
-        request = self.factory.post('/api/people/', data, format='json')
-        force_authenticate(request, user=self.user)
-        request = Request(request)
-        page_number = self.pagination.get_page_number(request, self.pagination)
-        self.assertTrue(isinstance(page_number, int))
-        self.assertTrue(page_number >= 1)
+    @given(st.dictionaries(
+        keys=st.sampled_from(['type',
+                              'page',
+                              'page_size',
+                              'sort_by',
+                              'sort_direction',
+                              'filter',
+                              'alive',
+                              'sex',
+                              'age_min',
+                              'age_max',
+                              'is_ttu',
+                              'is_ff']),
+        values=st.one_of(
+            st.none(),
+            # age values between 1 and 99
+            st.integers(min_value=1, max_value=99),
+            # predefined fullnames
+            st.sampled_from(["Ivan*", "Andrey*", "Svetlana*"]),
+            # True/False values for alive, is_ttu, is_ff
+            st.booleans()
+        )
+    ))
+    def test_valid_page_request(self, request_data):
+        client = APIClient()
+        for _ in range(10):
+            response = client.post('/people', request_data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    @given(dictionaries(text(min_size=5, max_size=20), integers(min_value=1, max_value=100)))
-    def test_get_page_size(self, data):
-        request = self.factory.post('/api/people/', data, format='json')
-        force_authenticate(request, user=self.user)
-        request = Request(request)
-        page_size = self.pagination.get_page_size(request)
-        self.assertTrue(isinstance(page_size, int))
-        self.assertTrue(self.pagination.min_page_size <= page_size <= self.pagination.max_page_size)
-
+    @given(st.dictionaries(
+        keys=st.sampled_from(['type',
+                              'page',
+                              'page_size',
+                              'sort_by',
+                              'sort_direction',
+                              'filter',
+                              'alive',
+                              'sex',
+                              'age_min',
+                              'age_max',
+                              'is_ttu',
+                              'is_ff']),
+        values=st.one_of(
+            st.none(),
+            # age values greater than 99 (invalid)
+            st.integers(min_value=100),
+            # any text for filter
+            st.text(),
+            # True/False values for alive, is_ttu, is_ff
+            st.booleans()
+        )
+    ))
+    def test_invalid_page_request(self, request_data):
+        client = APIClient()
+        for _ in range(10):
+            response = client.post('/people', request_data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
