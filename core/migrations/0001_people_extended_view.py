ff6d0658a5409b228ef00087f76b36eb7c6123dd
Yurii Cherkasov <strategarius@protonmail.com>
2023-12-14 14:18:18 +0700

    Migration script

--- core/migrations/0001_people_extended_view.py
+++ core/migrations/0001_people_extended_view.py
@@ -19,10 +19,44 @@ def create_migration(directory):
 
 class Migration(migrations.Migration):
     """
-    View comprises data on the relevant patients, both from the people table, and elsewhere in the DB
+    View comprises data on the relevant patients, both from the people table,
+    and elsewhere in the DB
     """
+    
     dependencies = [
-        'people', 'people_on_photos', 'photos'
+        'organization_type',
+        'organizations',
+        'media_segments',
+        'youtube_channels',
+        'rutube_channels',
+        'media_roles',
+        'ntv_episodes',
+        'komso_episodes',
+        'smotrim_episodes',
+        'youtube_vids',
+        'rutube_vids',
+        'people',
+        'websites',
+        'theory',
+        'quotes',
+        'photos',
+        'people_bundles',
+        'days_of_war',
+        'telegram_channels',
+        'telegram_authors',
+        'text_media',
+        'printed',
+        'printed_to_people_mapping',
+        'youtube_authors',
+        'people_to_msegments_mapping',
+        'people_on_youtube',
+        'people_on_smotrim',
+        'people_on_photos',
+        'people_in_orgs',
+        'people_in_bundles',
+        'people_3rdprt_details_raw',
+        'msegments_to_ychannels_mapping',
+        'msegments_to_rchannels_mapping',
     ]
 
     operations = create_migration(SQL_DIR)
